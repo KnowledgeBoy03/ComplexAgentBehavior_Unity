a57f6a0317c4a3eba2e79b1201996511f65af1d3
KnowledgeBoy03 <57725767+KnowledgeBoy03@users.noreply.github.com>
2020-05-13 00:16:06 -0400

    Preliminary Trigger Detect and Speaker Script
    Put down WIP trigger detection for the pursuers and a speaker script.

--- Assets/Scripts/pursuerScript.cs
+++ Assets/Scripts/pursuerScript.cs
@@ -1,18 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class pursuerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    CapsuleCollider detection;
+    NavMeshAgent agent;
+    GameObject speaker;
+    public bool found;
+
     void Start()
     {
-        
+        detection = GetComponent<CapsuleCollider>();
+        agent = GetComponent<NavMeshAgent>();
+        GameObject speaker = GameObject.FindGameObjectWithTag("Speaker");
+        found = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (found)
+        {
+            detection.radius += 6.0f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Evader"))
+        {
+            found = true;
+            agent.destination = other.transform.position;
+        }
+        if (other.CompareTag("Pursuer"))
+        {
+            if (found)
+            {
+                other.gameObject.GetComponent<CapsuleCollider>().radius += 6.0f;
+                other.gameObject.GetComponent<NavMeshAgent>().destination = agent.destination;
+                other.gameObject.GetComponent<pursuerScript>().found = true;
+            }
+        }
+        if (other.CompareTag("Speaker"))
+        {
+            found = true;
+            agent.destination = other.gameObject.GetComponent<speakerScript>().evaderPos.position;
+        }
+    }
+
+    private void PlaySpeaker()
+    {
+        speaker.GetComponent<speakerScript>().Reveal();
     }
 }
